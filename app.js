a85da5892012b098b3fe77ac270ed6475c2b0f32
Arber Shabani <arber.shabani@hotmail.com>
2018-10-30 05:19:15 +0000

    HTTP/HTTPS serving

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express'),
-      app = express();
-// const keys = require('./config/keys');
-const keys = require('./config.example/keys');
+      app = express(),
+      keys = require('./config/keys');
 
 // Services
 require('./services/sample');
@@ -20,7 +19,26 @@ if (process.env.NODE_ENV==='production') {
   });
 }
 
+
+// Development Serving
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port`, PORT);
 });
+
+// HTTP/HTTPS Setup
+/*
+    const http = require("http"),
+          https = require("https"),
+          fs = require("fs");
+          
+    var options = {
+      key: fs.readFileSync('./ssl/keys/key.pem'),
+      cert: fs.readFileSync('./ssl/certs/cert.cert')
+    };
+    
+    // Create an HTTP service.
+    http.createServer(app).listen(80);
+    // Create an HTTPS service identical to the HTTP service.
+    https.createServer(options, app).listen(443);
+*/
